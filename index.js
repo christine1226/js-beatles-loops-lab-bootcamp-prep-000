a466ccdc56f15196f9b084ee7329de7b3030e7f0
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-31 00:38:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ var array=[];
   }
   return array
 }
-var fact=["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+var facts=["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function johnLennonFacts(facts){
-  return facts;
+  var i=0
+  var arr=[];
+  while(i<5){arr.push(`${facts[i]}!!!`) i++}
+  return arr;
 }
