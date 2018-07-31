05961ff08f007ea16f054d516eb513da009727bd
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-31 00:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var array=[];
 }
 var fact=["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function johnLennonFacts(facts){
-  var i=0
-  while(){facts[i] + "!!!";}
+  while(){facts[] + "!!!";}
   return facts;
 }
