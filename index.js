edce36c1c542511ca0d02c6df8d42f3b42db3f81
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-31 00:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ var array=[];
   }
   return array
 }
+var fact=["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+function johnLennonFacts(facts){
+  var i=0
+  while(){facts[i] + "!!!";}
+  return facts;
+}
