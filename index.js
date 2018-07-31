efe43af122e11d009ffb690e15ec6f09da442072
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-31 00:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments=["Guitar", "Bass guitar", "Lead guitar", "Drums"];
+
+function theBeatlesPlay(musicians, intstruments){
+var array=[];
+  for (var i=0; i<musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
